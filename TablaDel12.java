4d80534160b9c627b3dd900ac0bfec7ee8073786
Juan Alfaro <akjfdsjaioej334@gmail.com>
2026-01-14 10:41:21 +0100

    incluyo cambio solicitado de esta es la tabla del 12

--- TablaDel12.java
+++ TablaDel12.java
@@ -5,6 +5,7 @@ public class TablaDel12
         for(int i = 0; i <= 10; i++)
         {
             System.out.println("12 * "+ i + " = " + 12 * i + "\n");
+            System.out.println("Esta es la tabla del 12");
         }
     }
 }
