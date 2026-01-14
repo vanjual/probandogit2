e7a7c2c301b7a8a3d59c177ae3bcb3404a5cd0f3
Lorena Gallego <ladfjierjekfj4568@gmail.com>
2026-01-14 10:50:43 +0100

    incluyo cambio solicitado En el siguiente fichero estará la tabla del 13

--- TablaDel12.java
+++ TablaDel12.java
@@ -5,7 +5,7 @@ public class TablaDel12
         for(int i = 0; i <= 10; i++)
         {
             System.out.println("12 * "+ i + " = " + 12 * i + "\n");
-            System.out.println("Se ha imprimido por pantalla la tabla del 12.");
+            System.out.println("En el siguiente fichero estará la tabla del 13.");
         }
     }
 }
